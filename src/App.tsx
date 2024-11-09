8b393bae4e2984594c8655c5c9b949ad22e072b0
chihiro_imac <ppgeswimmer48de@icloud.com>
2024-11-09 15:08:20 +0900

    mui（emotion）関係の追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+/** @jsxImportSource @emotion/react */
+import { jsx, css } from '@emotion/react';
+import styled from '@emotion/styled';
+
 function App() {
+  const Wrapeer = styled.div`
+    margin:0;
+    background-color: #a0c0f0
+  `
 
   return (
     <>
-      <p>first</p>
+      <Wrapeer>
+        <p>first</p>
+      </Wrapeer>
     </>
   )
 }
