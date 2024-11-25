28a520277306080dec779c210bd3121d64eea3f4
KobayashiChihiro <c_kobayashi@imin.co.jp>
2024-11-25 23:21:31 +0900

    se2ライン化・5フェードボタン追加

--- src/components/MainContents.tsx
+++ src/components/MainContents.tsx
@@ -1,5 +1,5 @@
-import styled from '@emotion/styled';
-import StandardAudioPlayerPanel from './StandardAudioPlayerPanel';
+import styled from "@emotion/styled";
+import StandardAudioPlayerPanel from "./StandardAudioPlayerPanel";
 
 //-----音声import---------
 // import sound1 from '../assets/TOTEMO_HEN_NA_OTO.mp3';
@@ -8,45 +8,42 @@ import StandardAudioPlayerPanel from './StandardAudioPlayerPanel';
  * 音声コンテンツをまとめる最上位
  */
 const MainContents = () => {
-
   // 音声コンテンツ全体のスタイル
   const MainContentsWrap = styled.div`
     display: flex;
     flex-direction: row;
-  `
+  `;
 
   // 音声コンテンツの各エリア（BGM・SE各エリア）を分けるスタイル
   const MainContentBGMArea = styled.div`
-    border: solid 1px #321e06;
-    width:37%;
+    border: solid 0.5px #321e06;
+    width: 28%;
     height: 85vh;
     padding: 1em;
     overflow-x: scroll;
     display: flex;
     flex-direction: column;
     scrollbar-width: none;
-  `
+  `;
   const MainContentSEArea = styled.div`
-    border: solid 1px #321e06;
-    width:26%;
+    border: solid 0.5px #321e06;
+    width: 22%;
     height: 85vh;
     padding: 1em;
     overflow-x: scroll;
     display: flex;
     flex-direction: column;
     scrollbar-width: none;
-  `
+  `;
 
   // BGMのライン
   const bgms = (
     <>
-      {/* ここにスクリプトで作ったコードを貼り付け */}
     </>
   );
 
   const ses = (
     <>
-      {/* ここにスクリプトで作ったコードを貼り付け */}
     </>
   );
 
@@ -56,21 +53,25 @@ const MainContents = () => {
         <MainContentBGMArea>
           {/* bgm1 */}
           {bgms}
-
         </MainContentBGMArea>
 
         <MainContentBGMArea>
           {/* bgm2 */}
           {bgms}
-
         </MainContentBGMArea>
+
+        <MainContentSEArea>
+          {/* se */}
+          {ses}
+        </MainContentSEArea>
+
         <MainContentSEArea>
           {/* se */}
           {ses}
         </MainContentSEArea>
       </MainContentsWrap>
     </>
-  )
-}
+  );
+};
 
-export default MainContents
+export default MainContents;
--- src/components/StandardAudioPlayerPanel.tsx
+++ src/components/StandardAudioPlayerPanel.tsx
@@ -1,9 +1,8 @@
-import { useState } from 'react';
-import styled from '@emotion/styled';
-import AudioControlButton from './AudioControlButton';
-import useSound from 'use-sound';
-import { delay } from '../utilFunction/delay';
-
+import { useState } from "react";
+import styled from "@emotion/styled";
+import AudioControlButton from "./AudioControlButton";
+import useSound from "use-sound";
+import { delay } from "../utilFunction/delay";
 
 // アイコン群
 import { FaPlay } from "react-icons/fa";
@@ -11,22 +10,36 @@ import { FaTurnUp } from "react-icons/fa6";
 import { FaTurnDown } from "react-icons/fa6";
 import { IoStopSharp } from "react-icons/io5";
 import { RiAlertFill } from "react-icons/ri";
-import { IconContext } from 'react-icons';
+import { IconContext } from "react-icons";
+import { FaAnglesDown } from "react-icons/fa6";
+import { FaAnglesUp } from "react-icons/fa6";
 
 export type AudioProps = {
-  sounds: string[],
+  sounds: string[];
   defVol: number;
   dispName: string;
-  bgColor: 'case1' | 'case2' | 'case3' | 'case4' | 'case5' | 'case6' | 'case7' | 'casese',
-}
+  bgColor:
+    | "case1"
+    | "case2"
+    | "case3"
+    | "case4"
+    | "case5"
+    | "case6"
+    | "case7"
+    | "casese";
+};
 
 /**
  * 汎用の音声再生パネル 1再生1パネルで使用する
  * 音源は1個以上N個以下受け取れる
  * 1曲目が終了したら2曲目を流す、それをクロスフェード……できる？
  */
-const StandardAudioPlayerPanel = ({ sounds, defVol, dispName, bgColor }: AudioProps) => {
-
+const StandardAudioPlayerPanel = ({
+  sounds,
+  defVol,
+  dispName,
+  bgColor,
+}: AudioProps) => {
   // ------音声コントロール------------------------------------------------------------
   const [isPlaying, setIsPlaying] = useState(false);
   const [play, { stop, sound, duration }] = useSound(sounds[0]); // volumeはショートハンドな書き方してるだけ
@@ -43,65 +56,69 @@ const StandardAudioPlayerPanel = ({ sounds, defVol, dispName, bgColor }: AudioPr
       setIsPlaying(true);
       play();
     }
-  }
+  };
 
   // -------曲長の表示-------------------------------------------------------------
-  const dispDurationMin = duration ? Math.floor(duration / 1000 / 60) : '';
-  const dispDurationSec = duration ? Math.round(duration % (1000 * 60) / 1000) : '';
+  const dispDurationMin = duration ? Math.floor(duration / 1000 / 60) : "";
+  const dispDurationSec = duration
+    ? Math.round((duration % (1000 * 60)) / 1000)
+    : "";
   // -------フェード中に操作させない------------------------------------------
   const [isFading, setIsFading] = useState(false);
   // -----------------------------------------------------------------------------
 
   // パネル背景色
   const bgColors = {
-    case1: '#9dc2e3',
-    case2: '#fae4d6',
-    case3: '#e2efda',
-    case4: '#d4d8f9',
-    case5: '#d0cece',
-    case6: '#e8cfcb',
-    case7: '#dc8a8a',
-    casese: '#beb5c9',
-  }
+    case1: "#9dc2e3",
+    case2: "#fae4d6",
+    case3: "#e2efda",
+    case4: "#d4d8f9",
+    case5: "#d0cece",
+    case6: "#e8cfcb",
+    case7: "#dc8a8a",
+    casese: "#beb5c9",
+  };
   const bgChoseColor = bgColors[bgColor];
 
   // パネル全体のスタイル
   const transparency = isPlaying ? 1 : 0;
   const StandardAudioPlayerPanelWrap = styled.div`
-      width: calc(100% -2em);
-      padding: 1em;
-      margin: 0.5em 0 0.5em 0;
-      display: flex;
-      flex-direction: column;
-      border: solid 0.3em rgb(255 60 60 / ${transparency});
-    `
+    width: calc(100% -2em);
+    padding: 1em;
+    margin: 0.5em 0 0.5em 0;
+    display: flex;
+    flex-direction: column;
+    border: solid 0.3em rgb(255 60 60 / ${transparency});
+  `;
 
   // ボタン横配置部分のスタイル
   const AudioControlButtonWrap = styled.div`
-      padding: 1em 0 0 0;
-      display: flex;
-      flex-direction: row;
-      justify-content: space-between;
-    `
+    padding: 1em 0 0 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  `;
 
   // 文字情報スタイル
   const DispInfomationWrap = styled.div`
-      display: flex;
-      flex-direction: row;
-      justify-content: space-between;
-    `
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  `;
 
   // 緊急音量ゼロボタン
   const EmergencyVolZeroBtn = styled.button`
-      padding: 0.5em 0 0.5em 0;
-    `
+    padding: 0.5em 0 0.5em 0;
+  `;
 
   return (
     <>
       <StandardAudioPlayerPanelWrap style={{ backgroundColor: bgChoseColor }}>
         <DispInfomationWrap>
           <p>{dispName}</p>
-          <p>{dispDurationMin}:{dispDurationSec}</p>
+          <p>
+            {dispDurationMin}:{dispDurationSec}
+          </p>
         </DispInfomationWrap>
 
         <p>音量初期値:{defVol}</p>
@@ -115,40 +132,66 @@ const StandardAudioPlayerPanel = ({ sounds, defVol, dispName, bgColor }: AudioPr
           <AudioControlButton
             icon={isPlaying ? <FaTurnDown /> : <FaTurnUp />}
             isFading={isFading}
-            onClick=
-            {isPlaying
-              ? (async () => {
-                // フェードアウト停止
-                setIsFading(true);
-                sound.fade(defVol, 0, 1000 * 30);
-                await delay(1000 * 30);
-                setIsFading(false);
-                // もしフェードアウト中に別で停止状態になったらフェードアウト処理をリセットしないといけない
-                // howlerjsだけで行った方がよかったんでない？
-                if (isPlaying) {
-                  handlePlaying() //  再生状態の時のみ停止操作
-                };
-              })
-              : (async () => {
-                // フェードイン
-                handlePlaying(); // 音量が初期値で再生されるけど速攻でフェード処理入る（ひでぇ）
-                setIsFading(true);
-                sound.fade(0, defVol, 1000 * 25);
-                await delay(1000 * 25);
-                setIsFading(false);
-              })
+            onClick={
+              isPlaying
+                ? async () => {
+                    // フェードアウト停止
+                    setIsFading(true);
+                    sound.fade(defVol, 0, 1000 * 30);
+                    await delay(1000 * 30);
+                    setIsFading(false);
+                    // もしフェードアウト中に別で停止状態になったらフェードアウト処理をリセットしないといけない
+                    // howlerjsだけで行った方がよかったんでない？
+                    if (isPlaying) {
+                      handlePlaying(); //  再生状態の時のみ停止操作
+                    }
+                  }
+                : async () => {
+                    // フェードイン
+                    handlePlaying(); // 音量が初期値で再生されるけど速攻でフェード処理入る（ひでぇ）
+                    setIsFading(true);
+                    sound.fade(0, defVol, 1000 * 25);
+                    await delay(1000 * 25);
+                    setIsFading(false);
+                  }
             }
           />
-          <EmergencyVolZeroBtn onClick={(() => sound.volume(0))}>
-            <IconContext.Provider value={{ size: '1.2em', color: '#f05050' }}>
+          <AudioControlButton
+            icon={isPlaying ? <FaAnglesDown /> : <FaAnglesUp />}
+            isFading={isFading}
+            onClick={
+              isPlaying
+                ? async () => {
+                    // フェードアウト停止（短スパン）
+                    setIsFading(true);
+                    sound.fade(defVol, 0, 1000 * 5);
+                    await delay(1000 * 5);
+                    setIsFading(false);
+                    // もしフェードアウト中に別で停止状態になったらフェードアウト処理をリセットしないといけない
+                    // howlerjsだけで行った方がよかったんでない？
+                    if (isPlaying) {
+                      handlePlaying(); //  再生状態の時のみ停止操作
+                    }
+                  }
+                : async () => {
+                    // フェードイン（短スパン）
+                    handlePlaying(); // 音量が初期値で再生されるけど速攻でフェード処理入る（ひでぇ）
+                    setIsFading(true);
+                    sound.fade(0, defVol, 1000 * 5);
+                    await delay(1000 * 5);
+                    setIsFading(false);
+                  }
+            }
+          />
+          <EmergencyVolZeroBtn onClick={() => sound.volume(0)}>
+            <IconContext.Provider value={{ size: "1.2em", color: "#f05050" }}>
               <RiAlertFill />
             </IconContext.Provider>
           </EmergencyVolZeroBtn>
-
         </AudioControlButtonWrap>
       </StandardAudioPlayerPanelWrap>
     </>
-  )
-}
+  );
+};
 
-export default StandardAudioPlayerPanel
+export default StandardAudioPlayerPanel;
